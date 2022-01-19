e2bcc41978f758f62ef590385cda67f424e9d862
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-01-19 07:39:18 -0500

    Add files via upload

---
+++ Complete.cpp
@@ -0,0 +1,44 @@
+// Loop and a Half Notes
+//  I CAN use a BREAK statement to exit a loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Example (from the previous notes)
+    cout << "Enter numbers (-1 to stop): " << endl;
+    int num = 0;
+
+    while (num != -1) {
+        cin >> num;
+
+        if (num % 2 == 0) {
+            cout << "EVEN" << endl;
+        }
+        else {
+            cout << "ODD" << endl;
+        }
+    }
+
+    //A different way to approach the same problem
+    cout << endl << endl << "Enter numbers (-1 to stop): " << endl;
+    num = 0;
+
+    while (true) { // loop forever
+        cin >> num;
+
+        if (num == -1) {
+            break; // break exits the current loop
+        }
+
+        if (num % 2 == 0) {
+            cout << "EVEN" << endl;
+        }
+        else {
+            cout << "ODD" << endl;
+        }
+    }
+
+    system("pause");
+}
