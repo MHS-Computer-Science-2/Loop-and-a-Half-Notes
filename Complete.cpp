14b8de8918c3b6e4cf8418c3e3466c8f70e9a0c1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-01-31 08:01:16 -0500

    Update Complete.cpp

--- Complete.cpp
+++ Complete.cpp
@@ -6,12 +6,11 @@ using namespace std;
 
 int main()
 {
-    //Example 1 (from the previous notes)
-    // The loop itself controls when it stops
+    //Example (from the previous notes)
     cout << "Enter numbers (-1 to stop): " << endl;
     int num = 0;
 
-    while (num != -1) { //Loop as long as num is NOT -1
+    while (num != -1) { // what is true to continue
         cin >> num;
 
         if (num % 2 == 0) {
@@ -22,18 +21,15 @@ int main()
         }
     }
 
-    //EXAMPLE 2
-    // A different way to approach the same problem
-    // The loop is set to go on forever, and we use an if-statement to end the loop
-    // This is called a LOOP AND A HALF, because the loop will end half way through its code
+    //A different way to approach the same problem
     cout << endl << endl << "Enter numbers (-1 to stop): " << endl;
     num = 0;
 
-    while (true) { // Loop forever
+    while (true) { // means loop forever
         cin >> num;
 
-        if (num == -1) {
-            break; // break immediately exits the current loop
+        if (num == -1) { // what should be true to stop
+            break; //immediately ends the loop
         }
 
         if (num % 2 == 0) {
@@ -43,11 +39,6 @@ int main()
             cout << "ODD" << endl;
         }
     }
-    
-    //The two examples accomplish the same task
-    //Sometimes a solution is EASIER to think about using a LOOP AND A HALF
-    //   - In Example 1, we must think of what should be true to KEEP GOING
-    //   - In Example 2, we must think of what should be true to STOP
 
     system("pause");
 }
